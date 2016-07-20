851f53eee70e016bbb90b8fda9100d3bd474eede
Michael Molinari <MPM@MPM-MacBook.local>
2016-07-19 22:48:58 -0700

    updated about me end of day 2

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-var userName = prompt('What is your name?');
+var userName = prompt('Welcome! What is your name?');
 
-alert('Hi, ' + userName + ', welcome! Let\'s see how much you already know about me! Please answer true or false');
+alert('Thank you for visiting my site, ' + userName + '! Let\'s see how much you already know about me! Please answer true or false');
 
-var answer1 = prompt('I am from West Virginia?');
+var answer1 = prompt('I was born on the same day Julius Caesar was assassinated.');
 
-if (answer1 === 'True' || answer1 === 'true') {
-  alert('That is right, ' + userName + '! I was born and raised in West Virginia.');
+if (answer1.toLowerCase() === 'true' || answer1.toLowerCase() === 't') {
+  alert('Great guess, ' + userName + '! I was born on the Ides of March.');
 } else {
-  alert('Sorry, ' + userName + ', no bueno. I was born and raised in West Virginia.');
+  alert('Sorry, ' + userName + ', no bueno. I was born on March 15.');
 }
 
-var answer2 = prompt('I am the youngest in my family?');
+var answer2 = prompt('I am from West Virginia.');
 
-if (answer2 === 'True' || answer2 === 'true') {
-  alert('Correct! I am the youngest. My siblings are Kate, Michelle, Stephen, and Lawrence.');
+if (answer2.toLowerCase() === 'true' || answer2.toLowerCase() === 't') {
+  alert('Excellent! I was born and raised in West Virginia.');
 } else {
-  alert('I wish. I was bullied by my four older siblings growing up.');
+  alert('Sorry, ' + userName + ', that is wrong. I was born and raised in West Virginia.');
 }
 
-var answer1 = prompt('I played basketball at West Virginia University?');
+var answer3 = prompt('I played basketball at West Virginia University.');
 
-if (answer1 === 'False' || answer1 === 'false') {
+if (answer3.toLowerCase() === 'false' || answer3.toLowerCase() === 'f') {
   alert('That\'s right! Although I am a great hooper, I played football for the Mountaineers.');
 } else {
-  alert('Not a true fan... that is wrong. I was a kicker on the football team at WVU.');
+  alert('Not a fan, I suppose. I was a kicker on the football team at WVU.');
 }
 
-var answer1 = prompt('Italian cuisine is my favorite?');
+var answer4 = prompt('Italian cuisine is my favorite.');
 
-if (answer1 === 'True' || answer1 === 'true') {
-  alert('Bravo Bravo! Chicken Alfredo is my favorite.');
+if (answer4.toLowerCase() === 'true' || answer4.toLowerCase() === 't') {
+  alert('Bravo Bravo! Especially Chicken Alfredo.');
 } else {
   alert('Non buono, ' + userName + '. I could eat pasta everyday for the rest of my life.');
 }
 
-var answer1 = prompt('I have always wanted to be a pilot?');
+var answer5 = prompt('I have always wanted to be a pilot.');
 
-if (answer1 === 'True' || answer1 === 'true') {
+if (answer5.toLowerCase() === 'true' || answer5.toLowerCase() === 't') {
   alert('You know me well, ' + userName + '! I have always loved flying.');
 } else {
   alert('This is true. I still aspire to be a pilot one day.');
